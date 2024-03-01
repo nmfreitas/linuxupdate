35a14705d1cffa3449f1c569c441f69bb3dff683
nmfreitas <86863705+nmfreitas@users.noreply.github.com>
2024-03-01 16:11:08 +0000

    Update linuxupdate.sh

--- linuxupdate.sh
+++ linuxupdate.sh
@@ -1,23 +1,28 @@
 #! /bin/bash
 
 pc=$(hostname)
-update_output=$(sudo apt update 2>&1)
+update_output=$(sudo apt update )
+upgrade_output=$(sudo sudo apt upgrade -y )
 
 #check for updates
 if [[ $(sudo apt update | grep -o '^[0-9]\+ packages can be upgraded') ]]; then
-    echo "Updates are available. Proceeding with the update..."
     sudo apt upgrade -y
     sudo apt autoremove -y
-    echo "System update completed."
-        if [[ $update_output =~ "W: An error occurred" ]]; then
-    echo "Error occurred during update."
-    echo "$update_output"  # Print the error messages
-    curl -d "Update error on machine $pc" https://noti.techtretas.dev/teste  # Send a notification
+    #echo "parte1"
+
+    if [[ $update_output =~ "W:" ]]; then # Check warnings
+        #echo "$update_output"  # Print the error messages
+        curl -d "Updates with warnings on machine $pc" https://noti.techtretas.dev/alerts  # Send a notification
+        #echo "warnings"
+    elif [[ $upgrade_output =~ "The following " ]]; then # Check not upgraded
+        #echo "$upgrade_output"  # Print the error messages
+        curl -d "Upgrade needed on machine $pc" https://noti.techtretas.dev/alerts  # Send a notification 
+        #echo "Upgrades needed"
+    elif [[$upgrade_output != "The following " | $update_output != "W:" ]]; then 
+        curl -d "Updates OK on machine $pc" https://noti.techtretas.dev/alerts
+        #echo "Updates are OK on machine $pc"
+    fi
 else
-        curl -d "Feitos updates a maquina $pc" https://noti.techtretas.dev/teste ## envia push
-       
- fi
-else
-    echo "No updates are available."
-    curl -d "Maquina $pc sem updates" https://noti.techtretas.dev/teste
+    #echo "No updates are available."
+    curl -d "Machine $pc without updates" https://noti.techtretas.dev/alerts # Send a notification
 fi
