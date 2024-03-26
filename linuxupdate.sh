8330d755d91adbe6053e33be1cf7881c2badc9fd
nmfreitas <86863705+nmfreitas@users.noreply.github.com>
2024-03-26 13:21:05 +0000

    Update linuxupdate.sh

--- linuxupdate.sh
+++ linuxupdate.sh
@@ -14,14 +14,14 @@ if [[ $(sudo apt update | grep -o '^[0-9]\+ packages can be upgraded') ]]; then
     if [[ $update_output =~ "W:" ]]; then # Check warnings
         #echo "$update_output"  # Print the error messages
         curl -d "Updates with warnings on machine $pc" https://noti.techtretas.dev/alerts  # Send a notification
-        #echo "warnings"
+        echo "warnings"
     elif [[ $upgrade_output =~ "The following " ]]; then # Check not upgraded
         #echo "$upgrade_output"  # Print the error messages
         curl -d "Upgrade needed on machine $pc" https://noti.techtretas.dev/alerts  # Send a notification 
-        #echo "Upgrades needed"
+        echo "Upgrades needed"
     elif [[$upgrade_output != "The following " | $update_output != "W:" ]]; then 
         curl -d "Updates OK on machine $pc" https://noti.techtretas.dev/alerts
-        #echo "Updates are OK on machine $pc"
+        echo "Updates are OK on machine $pc"
     fi
 else
     echo "No updates are available."
